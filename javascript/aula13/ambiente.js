2323dde1a7e8d8d82e206d980c47256381f49e9f
Oderlandio Junior <oderlandio.junior@outlook.com>
2026-07-07 12:39:52 -0300

    aprendendo sobre laços

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+
+/*Estrutura de repetição com teste lógico no final*/
+do {
+    console.log(`Passo ${c}`);
+    c++ // c = c + 1
+}while (c <= 6)
+
+/*Estrutura de repetição com teste lógico no inicio*/
+/*var c = 1
+while (c <= 3) {
+    console.log(`Passo ${c}`);
+    c++ // c = c + 1
+}*/
+
+/*console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");*/
