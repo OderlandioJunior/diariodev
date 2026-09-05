878415a07dfcbb43e1903c18afe30002d6aed9ec
Oderlandio Junior <oderlandio.junior@outlook.com>
2026-09-05 15:54:02 -0300

    fazendo a segunda parte do desafio

--- tech/javascript/aula13/repeticoesSimples.js
+++ tech/javascript/aula13/repeticoesSimples.js
@@ -7,11 +7,11 @@ do {
 }while (c <= 6)
 
 /*Estrutura de repetição com teste lógico no inicio*/
-/*var c = 1
+var c = 1
 while (c <= 3) {
     console.log(`Passo ${c}`);
     c++ // c = c + 1
-}*/
+}
 
 /*console.log("Tudo bem?");
 console.log("Tudo bem?");
